d250957bb1f2d26f19fc301f5b130e8465632e1b
Imam Hilmi <imam.hilmi253@gmail.com>
2022-02-01 16:53:59 +0700

    a

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -4,7 +4,18 @@ import "./Testimonials.scss"
 const Testimonials = () => {
   return (
     <div className="testimonials" id="testimonials">
-      Testimonials
+      <h1>Testimonials</h1>
+      <div className="container">
+        <div className="card">
+          <div className="top">
+            <img src="assets/right-arrow.png" alt="right arrow" />
+            <img src="" alt="" />
+            <img src="assets/youtube.png" alt="youtube" />
+          </div>
+          <div className="center"></div>
+          <div className="bottom"></div>
+        </div>
+      </div>
     </div>
     );
 };
